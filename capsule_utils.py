4713df3fac0d599749ead3daa357804d7620c97c
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2018-08-31 11:20:43 +0100

    Update capsule_utils.py

--- capsule_utils.py
+++ capsule_utils.py
@@ -6,13 +6,6 @@ import torch.nn as nn
 from pytorch_custom_utils import one_hot, Flatten
 
 
-# def squash(x):
-#     lengths2 = x.pow(2).sum(dim=2)
-#     lengths = lengths2.sqrt()
-#     x = x * (lengths2 / (1 + lengths2) / lengths).view(x.size(0), x.size(1), 1)
-#     return x
-
-
 def squash(input_tensor, dim=-1):
     """
     Applies norm non-linearity (squash) to a capsule layer.
